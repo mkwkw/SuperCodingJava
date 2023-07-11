913d92015f692ec845a728ca83c0818cd88be0c6
Kyoungwon Min <mkkw2047@gmail.com>
2023-07-11 18:28:59 +0900

    팀장님 지시 업무2

---
+++ SuperCodingJavaSpring/src/main/java/ChuncheonTicket3.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChuncheonTicket3 {
+
+    static class Person{
+        String name;
+        int age;
+        String nationality;
+
+        Person(String name, int age, String nationality){
+            this.name = name;
+            this.age = age;
+            this.nationality = nationality;
+        }
+    }
+
+    public static void main(String[] args) {
+        Person Alice = new Person("Alice", 70, "USA");
+        Person Kim = new Person("Kim", 65, "Korea");
+        Person Yoo = new Person("Yoo", 42, "Korea");
+
+        List<Person> people = new ArrayList<>();
+        people.add(Alice);
+        people.add(Kim);
+        people.add(Yoo);
+
+        for(Person person : people){
+            if(person.age>=65 && person.nationality=="Korea"){
+                System.out.println(person.name+"는 적용 대상자입니다.");
+            }
+            else{
+                System.out.println(person.name+"는 적용 대상자가 아닙니다.");
+            }
+        }
+    }
+}
