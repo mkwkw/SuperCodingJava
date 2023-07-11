92bb165e813492af15f208c80940f2f5e53c9cf8
Kyoungwon Min <mkkw2047@gmail.com>
2023-07-11 17:28:12 +0900

    팀장님 지시 업무1

---
+++ SuperCodingJavaSpring/src/main/java/ChuncheonTicket2.java
@@ -0,0 +1,11 @@
+public class ChuncheonTicket2 {
+    public static void main(String[] args){
+        int freeTickets = 1111;
+        int peopleCnt = freeTickets/4;
+        int remainCnt = freeTickets%4;
+
+        System.out.println(String.format("티켓을 가져갈 수 있는 근무자들 수는? %d명", peopleCnt));
+        System.out.println(String.format("티켓을 다 나눠주고, 남는 티켓 수는? %d장", remainCnt));
+
+    }
+}
