12de6781d3e46fd6262e7bdb609ecec16a810568
Kyoungwon Min <mkkw2047@gmail.com>
2023-07-11 16:35:21 +0900

    유니코드 변환

---
+++ SuperCoding-1/SuperCodingJavaSpring/src/main/java/UnicodeConversion.java
@@ -0,0 +1,15 @@
+public class UnicodeConversion {
+
+    public static void main(String[] args) {
+        System.out.println((char)50724);
+        System.out.println((char)45720);
+        System.out.println((char)46020);
+        System.out.println((char)32);
+        System.out.println((char)54868);
+        System.out.println((char)51060);
+        System.out.println((char)54021);
+        System.out.println((char)33);
+        //오늘도 화이팅!
+        //유니코드 변환
+    }
+}
