55f4d0c4288b2fdd17857fc4ad8181addf387d26
Kyoungwon Min <mkkw2047@gmail.com>
2023-07-12 14:28:05 +0900

    3주차 Day3 팀장님 지시 업무1

---
+++ SuperCodingJavaSpring/src/main/java/MovieAge.java
@@ -0,0 +1,26 @@
+public class MovieAge {
+
+    static String gradeByAge(int age){
+        if(age<0){
+            return "유아 관람 불가";
+        }
+        else if(age<12){
+            return "전체 관람가";
+        }
+        else if(age<15){
+            return "12세 이상 관람가";
+        }
+        else if(age<19){
+            return "15세 이상 관람가";
+        }
+        else{
+            return "청소년 관람불가";
+        }
+    }
+
+    public static void main(String[] args) {
+        for(int i=-1; i<20; i++){
+            System.out.println(gradeByAge(i));
+        }
+    }
+}
