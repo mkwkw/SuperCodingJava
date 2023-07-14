80a77af4316b443d84352ad3a4250ff87f835816
Kyoungwon Min <mkkw2047@gmail.com>
2023-07-14 16:09:43 +0900

    3주차 팀장님 지시 업무1 - 학생정보관리시스템

---
+++ SuperCodingJavaSpring/src/main/java/hw/Student.java
@@ -0,0 +1,39 @@
+package hw;
+
+public class Student {
+    private String name;
+    private int id;
+    private String major;
+    private int grade;
+
+    public Student(String name, int id, String major, int grade){
+        this.name = name;
+        this.id = id;
+        this.major = major;
+        this.grade = grade;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public String getMajor(){
+        return this.major;
+    }
+
+    public int getGrade(){
+        return this.grade;
+    }
+
+    public void setMajor(String major){
+        this.major = major;
+    }
+
+    public void setGrade(int grade){
+        this.grade = grade;
+    }
+}
---
+++ SuperCodingJavaSpring/src/main/java/hw/StudentManagementSystem.java
@@ -0,0 +1,51 @@
+package hw;
+
+import java.util.ArrayList;
+
+public class StudentManagementSystem {
+
+    private ArrayList<Student> arr;
+    int capacity;
+
+    public StudentManagementSystem(int capacity){
+        this.arr = new ArrayList<>();
+        this.capacity = capacity;
+    }
+
+    //학생 객체 추가
+    public void addStudent(Student student){
+        if(arr.size()<capacity){
+            arr.add(student);
+            System.out.println("학생 추가: "+student.getName());
+        }
+        else{
+            System.out.println("학생 데이터베이스가 가득 찼습니다.");
+        }
+    }
+
+    //학생 객체 삭제
+    public void removeStudent(Student student){
+        if(arr.contains(student)) {
+            arr.remove(student);
+        }
+        else{
+            System.out.println("학생을 찾을 수 없습니다.");
+        }
+    }
+
+    //힉생 객체 검색
+    public void searchStudent(int id){
+        for(Student student : arr){
+            if(student.getId()==id){
+                System.out.println("학생을 찾았습니다.");
+                System.out.println("이름: "+student.getName());
+                System.out.println("학번: "+student.getId());
+                System.out.println("전공: "+student.getMajor());
+                System.out.println("학년: "+student.getGrade());
+                return;
+            }
+        }
+
+        System.out.println("학생을 찾을 수 없습니다.");
+    }
+}
