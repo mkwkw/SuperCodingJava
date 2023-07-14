cda8274023db6b5cb7a4414ad795b2c2760aece4
Kyoungwon Min <mkkw2047@gmail.com>
2023-07-14 19:05:25 +0900

    팀장 지시 업무 - super, override 이용

---
+++ SuperCodingJavaSpring/src/main/java/hw/week3day5/Circle.java
@@ -0,0 +1,23 @@
+package hw.week3day5;
+
+import hw.week3day5.Shape;
+
+class Circle extends Shape {
+    private static final double PI = 3.14;
+    private double radius;
+
+    public Circle(String color, double radius) {
+        super(color); //Shape 생성자 이용
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return PI*radius*radius;
+    }
+
+    public void printCircleInfo() {
+        super.printInfo(); //Shape의 메서드 이용
+        System.out.println("원의 반지름: "+radius);
+    }
+}
---
+++ SuperCodingJavaSpring/src/main/java/hw/week3day5/Shape.java
@@ -0,0 +1,18 @@
+package hw.week3day5;
+
+class Shape {
+    protected String color;
+
+    protected Shape(String color) {
+        this.color = color;
+    }
+
+    public double getArea() {
+        return 0.0;
+    } //오버라이딩 됨.
+
+    protected void printInfo() {
+        System.out.println("도형의 색상: " + color);
+        System.out.println("도형의 면적: " + getArea());
+    }
+}
