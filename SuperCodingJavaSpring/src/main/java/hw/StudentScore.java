aed98a167f462adf716e0ea9ed83ae9095cc6d9f
Kyoungwon Min <mkkw2047@gmail.com>
2023-07-14 16:58:20 +0900

    3주차 Day5 사수 도움 요청 업무1 - 학생 성적 관리시스템

---
+++ SuperCodingJavaSpring/src/main/java/hw/StudentScore.java
@@ -0,0 +1,45 @@
+package hw;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class StudentScore {
+
+    // static
+    private static int serialIndex;
+    private static int[] allScores;
+
+    private int myIndex;
+    private int score;
+
+    public static int[] getAllScores() {
+        return allScores;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+        allScores[myIndex] = score;
+    }
+
+    public StudentScore(int score) {
+        this.score = score;
+        this.myIndex = serialIndex++; //전체 allScore의 길이++
+        addAllScore(score);
+    }
+
+    private static void addAllScore(int score) {
+        if(allScores==null){
+            allScores = new int[1];
+            allScores[0] = score;
+            return;
+        }
+        //else
+        int length = allScores.length;
+        allScores = Arrays.copyOf(allScores, length+1);
+        allScores[length] = score;
+    }
+}
