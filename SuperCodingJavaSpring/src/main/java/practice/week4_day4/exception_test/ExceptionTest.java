bb6aa25ad7ac1440b43155e95e4bf47f98ef448e
Kyoungwon Min <mkkw2047@gmail.com>
2023-07-24 23:18:52 +0900

    주석 수정

--- SuperCodingJavaSpring/src/main/java/practice/week4_day4/exception_test/ExceptionTest.java
+++ SuperCodingJavaSpring/src/main/java/practice/week4_day4/exception_test/ExceptionTest.java
@@ -3,6 +3,8 @@ package practice.week4_day4.exception_test;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ExceptionTest {
 
@@ -13,7 +15,7 @@ public class ExceptionTest {
         System.out.println("메인 메소드가 종료합니다.");
     }
 
-    public static void makeUncheckException(){ //UncheckException - 빨간줄 표시되지는 않음. - try-catch문으로 예외 처리 가능
+    public static void makeUncheckException(){ //UncheckException - 빨간줄 표시되지는 않음.
         int[] intArr={1,2,3,4,5,6,7,8,9,10};
         int index = 10;
 
@@ -32,6 +34,20 @@ public class ExceptionTest {
         catch (ArithmeticException e){
             System.out.println("0으로 나눌수가 없습니다.");
             e.printStackTrace();
+        }
+
+        List<String> wordList = new ArrayList<>();
+        wordList.add("hi");
+        wordList.add(null);
+        wordList.add("world");
+
+        for (int i=0; i<wordList.size(); i++){
+            try{
+                System.out.println(wordList.get(i));
+            }
+            catch (NullPointerException e){
+                System.out.println("null은 들어갈 수 없습니다.");
+            }
         }
     }
 
