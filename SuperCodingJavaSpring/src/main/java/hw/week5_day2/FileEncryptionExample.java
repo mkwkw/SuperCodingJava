a555e5fa564f30bd47c01cb8dace7e5eea13fb61
Kyoungwon Min <mkkw2047@gmail.com>
2023-07-28 16:37:11 +0900

    팀장님 지시 업무 - jasypt 이용 복호화

---
+++ SuperCodingJavaSpring/src/main/java/hw/week5_day2/FileEncryptionExample.java
@@ -0,0 +1,39 @@
+package hw.week5_day2;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.salt.StringFixedSaltGenerator;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileEncryptionExample {
+    public static void main(String[] args) throws IOException {
+        String encryptedOutputFilePath = "C:\\Users\\mkkw2\\Documents\\GitHub\\SuperCodingJavaSpring\\src\\main\\java\\hw\\week5_day2\\encrypted_abc.txt";
+        String decryptedOutputFilePath = "SuperCodingJavaSpring/src/main/java/hw/week5_day2/encrypted_abc.txt";
+
+        final String PW = "12341234";
+        final String ALGORITHM = "PBEWithMD5AndDES";
+        final String SALT_GENERATOR = "someFixedSalt";
+
+        // jasypt 라이브러리 이용해서 복호화
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setPassword(PW); //키
+        encryptor.setAlgorithm(ALGORITHM); //알고리즘
+        encryptor.setSaltGenerator(new StringFixedSaltGenerator(SALT_GENERATOR));
+
+        String text = encryptor.decrypt(readTextFile(encryptedOutputFilePath));
+        System.out.println("Decrypted text: "+text);
+    }
+
+    private static String readTextFile(String filePath) throws IOException {
+        StringBuilder content = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+        }
+        return content.toString();
+    }
+}
